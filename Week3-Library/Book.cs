1575e22b6c4557c02251ad0aefc61f9719eadfd0
Conor Dillon <Dillon-C23@ulster.ac.uk>
2024-10-08 10:57:31 +0100

    Updated code with variables and properties

--- Week3-Library/Book.cs
+++ Week3-Library/Book.cs
@@ -4,17 +4,39 @@ namespace Week3_Library
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
-        int Pages;
+        private string title;
+        private string author;
+        private string isbn;
+        private int pages;
 
+        // Title property to allow access
+        // to the title private variable
+        public string Title
+        {
+            get { return title; } // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+        public int Pages
+        {
+            get { return pages; }
+            set { pages = value; }
+        }
         public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
-            Pages = bookPages;
+            this.title = bookTitle;
+            this.author = bookAuthor;
+            this.isbn = bookISBN;
+            this.pages = bookPages;
         }
 
         public void DisplayInfo()
@@ -23,6 +45,7 @@ namespace Week3_Library
             Console.WriteLine($"Book Author: {Author}");
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine($"Number of pages: {Pages}");
+            Console.WriteLine("");
         }
     }
 }
