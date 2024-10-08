c61dfd4d17bcdcd47a1b70852fa0634ef7ba56d5
Conor Dillon <Dillon-C23@ulster.ac.uk>
2024-10-08 10:11:28 +0100

    Added number of pages to consstructor

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -3,12 +3,14 @@
     string Title;
     string Author;
     string ISBN;
+    int Pages;
 
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
+    public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
     {
         Title = bookTitle;
         Author = bookAuthor;
         ISBN = bookISBN;
+        Pages = bookPages;
     }
 
     void DisplayInfo()
@@ -17,13 +19,14 @@
         Console.WriteLine($"Book title: {Title}");
         Console.WriteLine($"Book Author: {Author}");
         Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"Number of pages: {Pages}");
     }
 
     static void Main(string[] args)
     {
         // create a new instance (object) of the Book class
         // note how the object name differs from the class name
-        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567",200);
 
         // Output the book information to the console
         book.DisplayInfo();
