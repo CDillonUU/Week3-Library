112e448e8470296ef7f23f19a150e67927cb6f31
Conor Dillon <Dillon-C23@ulster.ac.uk>
2024-10-08 11:20:27 +0100

    Updated code with a new Member class

---
+++ Week3-Library/Member.cs
@@ -0,0 +1,50 @@
+﻿namespace Week3_Library
+{
+    class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        // Public properties
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } // Private setter makes it read-only
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        // Consturctor for new member
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.memberId = memberId;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {memberId}");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"Phone Number: {phone}");
+            Console.WriteLine("");
+        }
+    }
+}
