d9851329bd9e35f02ece5aece12d383d715d534b
Conor Dillon <Dillon-C23@ulster.ac.uk>
2024-10-08 09:39:24 +0100

    Changes to code

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    static void Main(string[] args)
+    {
+        // create a new instance (object) of the Book class
+        // note how the object name differs from the class name
+        Book book = new Book();
+
+        // This information is for one book in our library
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        // Output the book information to the console
+        Console.WriteLine($"Book title: {book.Title}");
+        Console.WriteLine($"Book Author: {book.Author}");
+        Console.WriteLine($"Book ISBN: {book.ISBN}");
+    }
+}
